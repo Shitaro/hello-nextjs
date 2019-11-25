9d77fe26b9db12acde1b2dbc8860d023b787bb0e
shitaro <apprentice.physicist.shitaro@gmail.com>
2019-11-26 00:07:25 +0900

    Install TypeScript

--- pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = props => (
-    <div>Hello Next.js!</div>
-);
-
-export default Page;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NextPage } from "next";
+
+const Page: NextPage = () => (
+    <div>Welcome to next.js!</div>
+);
+
+export default Page;
